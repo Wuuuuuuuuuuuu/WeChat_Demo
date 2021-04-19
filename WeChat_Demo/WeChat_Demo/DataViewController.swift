ad3310c9742233e9c7d3e2c553ee49d09746519a
Wuuuuuu <1871138@stu.neu.edu.cn>
2021-04-19 16:24:34 +0800

    Build a project

---
+++ WeChat_Demo/WeChat_Demo/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  WeChat_Demo
+//
+//  Created by Wu QingBin on 2021/4/19.
+//  Copyright © 2021年 Wu QingBin. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
